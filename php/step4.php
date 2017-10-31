ff37ece3c9856916ff124b109b05e08db1ec2954
中山 梨奈 <nakayamarina@chuuzan-no-iMac-6.local>
2017-10-31 22:04:56 +0900

    update:what.html

--- php/step4.php
+++ php/step4.php
@@ -7,8 +7,7 @@ $sh_ex = "/bin/sh ../R_Python_sh/remove.sh";
 $output = shell_exec("{$sh_ex} 2>&1");
 //print_r($output);
 
-
 #最後に書く
-require('../html/step4.html');
+require('../html/step4.html')
 
 ?>
