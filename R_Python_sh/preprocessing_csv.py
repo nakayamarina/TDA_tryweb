d49f7cb4154f6af6708f0e63be75ae954f934559
nakayamarina <r.nakayama.kut@gmail.com>
2018-01-15 01:31:24 +0900

    speed up? by making read_data function

--- R_Python_sh/preprocessing_csv.py
+++ R_Python_sh/preprocessing_csv.py
@@ -1,164 +1,95 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
-######### データ読み込みおよび整形 #######################################
 
+##### データ読み込み ###############################################
 
-#引数取得
-args = sys.argv
+def read_data(row_name, column_name):
 
-#行名なし(0) or あり(1)
-row_name = int(args[1])
+    # 読み込むcsvファイル名
+    csv_file = '../get_data/get_data.csv'
 
-#列名なし(0) or あり(1)
-column_name = int(args[2])
-
-csv_file = '../get_data/get_data.csv'
-xlsx_file = '../get_data/get_data.xlsx'
-
-"""
-#行名なし　列名なし　csv
-if row_name == 0 and column_name == 0 and CorE == 0:
-    data = pd.read_csv(csv_file, index_col = None, header = None)
-
-    #欠損値削除
-    data = data.dropna()
-
-#行名なし　列名なし　xlsx
-elif row_name == 0 and column_name == 0 and CorE == 1:
-    data = pd.ExcelFile(xlsx_file)
-    #シート番号取得
-    sheets = data.sheet_names
-    #最初のシートをデータフレーム化
-    data = data.parse(sheets[0], index_col = None, header = None)
-    data = data.dropna()
-
-#行名あり　列名なし　csv
-elif row_name == 1 and column_name == 0 and CorE == 0:
-
-    data = pd.read_csv(csv_file, index_col = 0, header = None)
-    data = data.dropna()
-
-#行名あり　列名なし　xlsx
-elif row_name == 1 and column_name == 0 and CorE == 1:
-
-    data = pd.ExcelFile(xlsx_file)
-    #シート番号取得
-    sheets = data.sheet_names
-    #最初のシートをデータフレーム化
-    data = data.parse(sheets[0], index_col = 0, header = None)
-    data = data.dropna()
-
-#行名なし　列名あり　csv
-elif row_name == 0 and column_name == 1 and CorE == 0:
-
-    data = pd.read_csv(csv_file, index_col = None, header = 0)
-    data = data.dropna()
-
-#行名なし　列名あり　xlsx
-elif row_name == 0 and column_name == 1 and CorE == 1:
-
-    data = pd.ExcelFile(xlsx_file)
-    #シート番号取得
-    sheets = data.sheet_names
-    #最初のシートをデータフレーム化
-    data = data.parse(sheets[0], index_col = None, header = 0)
-    data = data.dropna()
-
-#行名あり　列名あり　csv
-elif row_name == 1 and column_name == 1 and CorE == 0:
-
-    data = pd.read_csv(csv_file, index_col = 0, header = 0)
-    data = data.dropna()
-
-#行名あり　列名あり　xlsx
-elif row_name == 1 and column_name == 1 and CorE == 1:
-
-    data = pd.ExcelFile(xlsx_file)
-    #シート番号取得
-    sheets = data.sheet_names
-    #最初のシートをデータフレーム化
-    data = data.parse(sheets[0], index_col = 0, header = 0)
-    data = data.dropna()
-
-"""
-if column_name == 0:
-
+    # 行名なし
     if row_name == 0:
+        idx = None
 
-        #行名なし　列名なし　csv
-        data = pd.read_csv(csv_file, index_col = None, header = None)
+    # 行名あり
+    else:
+        idx = 0
 
-        #欠損値削除
-        data = data.dropna()
+    # 列名なし
+    if column_name == 0:
+        hdr = None
 
+    # 列名あり
+    else:
+        hdr = 0
 
-    elif row_name == 1:
+    # データ読み込み
+    data = pd.read_csv(csv_file, index_col = idx, header = hdr)
+    # 欠損値削除
+    data = data.dropna()
 
-        #行名あり　列名なし　csv
-        if CorE == 0:
-
-            data = pd.read_csv(csv_file, index_col = 0, header = None)
-            data = data.dropna()
-
-elif column_name == 1:
-
-    if row_name == 0:
-        #行名なし　列名あり　csv
-        data = pd.read_csv(csv_file, index_col = None, header = 0)
-        data = data.dropna()
-
-    elif row_name == 1:
-
-        #行名あり　列名あり　csv
-        data = pd.read_csv(csv_file, index_col = 0, header = 0)
-        data = data.dropna()
-
-#列名をつける
-
-#1次元データの場合
-if data.shape[1] == 1:
-    data.columns = ['X']
-
-#2次元データの場合
-elif data.shape[1] == 2:
-    data.columns = ['X', 'Y']
-
-#整形データをcsvで出力
-data.to_csv('../get_data/pre_data.csv', index = False)
+    return(data)
 
 
-######### データの可視化 #######################################
+if __name__ == '__main__':
 
-#2次元データの場合
-if data.shape[1] == 2:
+    #引数取得
+    args = sys.argv
 
-    #X軸 : dataの1列目
-    #Y軸 : dataの2列目
-    plt.scatter(data.X, data.Y, c = "black")
+    #行名なし(0) or あり(1)
+    row_name = int(args[1])
 
-    #ラベルと目盛りの設定
-    plt.xlim(min(data.X)-2, max(data.X) + 2)
-    plt.ylim(min(data.Y)-2, max(data.Y) + 2)
-    plt.xlabel('X : Input Data 1st column')
-    plt.ylabel('Y : Input Data 2nd column')
+    #列名なし(0) or あり(1)
+    column_name = int(args[2])
 
-    #保存
-    plt.savefig('../get_data/user_data.png')
+    data = read_data(row_name, column_name)
 
-elif data.shape[1] == 1:
 
-    #X軸 : dataの個数(行数)
-    x = range(0,len(data))
+    ######### データの書き出し・可視化 ###############################
 
-    #Y軸 : data
-    plt.scatter(x, data.X, c = "black")
+    #2次元データの場合
+    if data.shape[1] == 2:
 
-    #ラベルと目盛りの設定
-    plt.xlim(-2, len(data) + 2)
-    plt.ylim(min(data.X)-2, max(data.X)+2)
-    plt.xlabel('X : Number of Input Data')
-    plt.ylabel('Y : Input Data')
+        # 列名をつける
+        data.columns = ['X', 'Y']
 
-    #保存
-    plt.savefig('../get_data/user_data.png')
+        # 整形データをcsvで出力
+        data.to_csv('../get_data/pre_data.csv', index = False)
+
+        #X軸 : dataの1列目
+        #Y軸 : dataの2列目
+        plt.scatter(data.X, data.Y, c = "black")
+
+        #ラベルと目盛りの設定
+        plt.xlim(min(data.X)-2, max(data.X) + 2)
+        plt.ylim(min(data.Y)-2, max(data.Y) + 2)
+        plt.xlabel('X : Input Data 1st column')
+        plt.ylabel('Y : Input Data 2nd column')
+
+        #可視化したものを保存
+        plt.savefig('../get_data/user_data.png')
+
+    elif data.shape[1] == 1:
+
+        # 列名をつける
+        data.columns = ['X']
+
+        # 整形データをcsvで出力
+        data.to_csv('../get_data/pre_data.csv', index = False)
+
+        #X軸 : dataの個数(行数)
+        x = range(0,len(data))
+
+        #Y軸 : data
+        plt.scatter(x, data.X, c = "black")
+
+        #ラベルと目盛りの設定
+        plt.xlim(-2, len(data) + 2)
+        plt.ylim(min(data.X)-2, max(data.X)+2)
+        plt.xlabel('X : Number of Input Data')
+        plt.ylabel('Y : Input Data')
+
+        #可視化したものを保存
+        plt.savefig('../get_data/user_data.png')
